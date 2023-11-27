a7991e4595ed147d6b807bb81d07520a7b6239b9
halifergbr <127866807+halifergbr@users.noreply.github.com>
2023-11-27 12:34:47 -0300

    xdb
    xbxcb

---
+++ nova-java-main/script.js
@@ -0,0 +1,6 @@
+javascript
+//* Exemplo de código JavaScript para o site*//
+
+exibirMensagem();{ alert("Bem-vindo ao Meu Site!");}
+
+// Event listener para chamar a função exibirMensagem quando o site for carregadowindow.addEventListener("load", exibirMensagem);
